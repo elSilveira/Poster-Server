103067a009a4b19f989bcbca5e886b5cadf94dd2
Eduardo Silveira <eduardo.luiz@ubanquity.com>
2023-03-22 20:58:15 -0300

    port

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ addRoutes()
 // Declare a route
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: 8080 })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
